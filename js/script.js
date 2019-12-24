03905561010bffa27bea3a53918badbe005ee531
faisalach <faisalachramsyah@gmail.com>
2019-12-24 13:33:22 +0700

    edit skill dan header

--- js/script.js
+++ js/script.js
@@ -16,28 +16,41 @@ $('.btnShow').on('click',function(e) {
 	var alamat = $(this).attr('href');
 
 });
+$('#contact').css({minHeight : $(window).height()+'px'});
 
 $(window).on('scroll',function(){
 	var scroll = $(this).scrollTop();
+
 	if ( scroll < 10 ) {
 		$('#navbar .navbar').removeClass('fixed');
 	} else{
 		$('#navbar .navbar').addClass('fixed');
 	}
 
-	if (scroll > 120) {
+	if (scroll > $('#skill').offset().top-200) {
+		$.each($('.skill .box'),function(i,j) {
+			setTimeout(function() {
+				j.classList.add('show');
+			},(i+1)*500);
+		})
+	}
+	if (scroll > $('#about').offset().top-200) {
 		$('#about .about-content').addClass('show');	
 	}
 
-	if (scroll > 500) {
+	if (scroll > $('#portfolio').offset().top-200) {
 		$('#portfolio .boxThumbnail').addClass('show');	
 		$('#portfolio .thumbnail').addClass('show');	
 	}
+	if (scroll > $('#contact').offset().top-200) {
+		$.each($('.contact .box'),function(i,j) {
+			setTimeout(function() {
+				j.classList.add('show');
+			},(i+1)*300);
+		})
+	}
 
-	if (scroll > $('#skill').offset().top-51) {
-		$('.nav-link').removeClass('text-warning');
-		$('.skill-link').addClass('text-warning');
-	}else if (scroll > $('#contact').offset().top-51) {
+	if (scroll > $('#contact').offset().top-51) {
 		$('.nav-link').removeClass('text-warning');
 		$('.contact-link').addClass('text-warning');
 	}else if(scroll > $('#portfolio').offset().top-51){
@@ -46,6 +59,9 @@ $(window).on('scroll',function(){
 	}else if(scroll > $('#about').offset().top-51){
 		$('.nav-link').removeClass('text-warning');
 		$('.about-link').addClass('text-warning');
+	}else if (scroll > $('#skill').offset().top-51) {
+		$('.nav-link').removeClass('text-warning');
+		$('.skill-link').addClass('text-warning');
 	}else{
 		$('.nav-link').removeClass('text-warning');
 	}
@@ -55,9 +71,13 @@ $(window).on('scroll',function(){
 
 $('.contact .box').hover(function() {
 	if ($(this).children().hasClass('show')) {
-		$(this).children().removeClass('show')
+		$(this).children().removeClass('show');
+		$(this).children().removeClass('color');
+		$(this).children()[0].classList.add('no-color');
 	}else{
 		$(this).children().addClass('show');
+		$(this).children()[0].classList.add('color');
+		$(this).children().removeClass('no-color');
 	}
 });
 
